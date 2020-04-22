66d406582b07246f240d202081cc206359e6c273
Ludvine <62137565+LudvineMegane@users.noreply.github.com>
2020-04-22 22:30:24 +0100

    Create Task.java
    creation of task class

---
+++ java/src/ie/tudublin/Task.java
@@ -0,0 +1,24 @@
+package ie.tudublin;
+
+public class Task
+{
+    private String name;
+    private int start;
+    private int end;
+
+    public String getName()
+{
+  return this.name;
+}
+public int getStart()
+{
+    return this.start;
+}
+
+public int getEnd()
+{
+    return this.end;
+}
+
+
+}
