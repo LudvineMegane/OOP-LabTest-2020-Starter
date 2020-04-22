d00c2d84e2ece69da248b2b0b6fcc587be6ab56f
Ludvine <62137565+LudvineMegane@users.noreply.github.com>
2020-04-22 22:55:20 +0100

    Update Task.java
    adding constructor

--- java/src/ie/tudublin/Task.java
+++ java/src/ie/tudublin/Task.java
@@ -6,19 +6,29 @@ public class Task
     private int start;
     private int end;
 
-    public String getName()
-{
-  return this.name;
-}
-public int getStart()
-{
-    return this.start;
-}
+    public Task(String name, int start, int end)
+    {
+        
+        this.name = name;
+        this.start = start;
+        this.end = end;
+    }
 
-public int getEnd()
-{
-    return this.end;
-}
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public int getStart()
+    {
+        return this.start;
+    }
+
+    public int getEnd()
+    {
+        return this.end;
+    }
 
 
 }
